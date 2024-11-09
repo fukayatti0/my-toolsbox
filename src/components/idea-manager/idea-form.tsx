4bcd8fbcbf1fa0dd6ad059d182183d02ac975844
fukayatti0 <st24162yk@ibaraki.kosen-ac.jp>
2024-11-09 21:17:58 +0900

    Idea Manager page: Change submit button to add frameworks field in IdeaForm

--- src/components/idea-manager/idea-form.tsx
+++ src/components/idea-manager/idea-form.tsx
@@ -337,10 +337,11 @@ const IdeaForm: React.FC<{
         ))}
       </div>
       <button
-        type="submit"
-        className="w-full bg-blue-500 text-white font-semibold py-3 px-6 rounded-lg mt-4"
+        type="button"
+        onClick={() => handleAddField("frameworks")}
+        className="w-full p-1 text-2xl bg-blue-500 text-white rounded-lg"
       >
-        追加
+        +
       </button>
     </form>
   );
