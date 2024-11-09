2db931b758cd8ca09a8ab05a7d97612f74deba21
fukayatti0 <st24162yk@ibaraki.kosen-ac.jp>
2024-11-09 18:10:40 +0900

    Refactor NextAuth handler to improve structure and export GET/POST methods

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,7 +2,7 @@
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
 
-export default NextAuth({
+const handler = NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_CLIENT_ID ?? '',
@@ -12,23 +12,25 @@ export default NextAuth({
           scope: 'read:user gist'  // gistスコープを追加
         }
       }
+        })
+      ],
+      callbacks: {
+        async jwt({ token, account }) {
+          // アクセストークンをJWTに保存
+          if (account) {
+            token.accessToken = account.access_token
+          }
+          return token
+        },
+        async session({ session, token }: { session: any; token: any }) {
+          // セッションにアクセストークンを追加
+          session.user.accessToken = token.accessToken
+          return session
+        }
+      },
+      pages: {
+        signIn: '/auth/signin',
+      },
     })
-  ],
-  callbacks: {
-    async jwt({ token, account }) {
-      // アクセストークンをJWTに保存
-      if (account) {
-        token.accessToken = account.access_token
-      }
-      return token
-    },
-    async session({ session, token }: { session: any; token: any }) {
-      // セッションにアクセストークンを追加
-      session.user.accessToken = token.accessToken
-      return session
-    }
-  },
-  pages: {
-    signIn: '/auth/signin',
-  },
-})
+    
+    export { handler as GET, handler as POST }
